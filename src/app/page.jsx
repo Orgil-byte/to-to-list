c9f60ecaef08fcf3e7d9a289695cbf46a9a56c3c
Orgil-byte <ulziitogtohorgil1908@gmail.com>
2026-01-19 18:10:34 +0800

    A

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,22 +8,12 @@ export default function Main() {
     </div>
   );
 }
-const Button = () => {
-  return <div></div>;
-};
 
 const Page = () => {
-  return (
-    <div className="min-h-screen w-full flex justify-center">
-      <Container />
-    </div>
-  );
-};
-
-const Container = () => {
-  const [line, setLine] = useState(false);
   const [input, setInput] = useState("");
   const [renderInput, setRenderInput] = useState([]);
+  const [nextId, setNextId] = useState(0);
+  const [filtered, setFiltered] = useState("all");
 
   const add = () => {
     if (input === "") {
@@ -32,13 +22,14 @@ const Container = () => {
     }
 
     const task = {
-      id: renderInput.length,
+      id: nextId,
       text: input,
       isComplete: false,
     };
 
     setRenderInput([...renderInput, task]);
     setInput("");
+    setNextId(nextId + 1);
   };
 
   const getInput = (e) => {
@@ -56,57 +47,25 @@ const Container = () => {
     setRenderInput(renderInput.filter((item) => item.id !== id));
   };
 
-  return (
-    <div>
-      <div className="mt-20  py-6 px-4 w-94.25 min-h-72.5 flex flex-col items-center shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
-        <h1 className="text-[20px] font-semibold">To-Do list</h1>
-        <div className="flex gap-1.5 mb-5 mt-5">
-          <input
-            placeholder="Add a new task..."
-            value={input}
-            onChange={getInput}
-            className="w-70 h-10 py-2 px-4 rounded-sm border border-[#E4E4E7]"
-          />
-          <button
-            onClick={add}
-            className="w-14.75 h-10 text-[13.3px] py-2 px-4 text-white bg-[#3C82F6] rounded-sm tracking-wider shadow-2xs cursor-pointer"
-          >
-            Add
-          </button>
-        </div>
-        <div className="flex w-86.25 gap-1.5 mb-5">
-          <button className="py-1 px-3 h-8 bg-[#F3F4F6] text-[#363636] text-[12px] rounded-sm">
-            All
-          </button>
-          <button className="py-1 px-3 h-8 bg-[#F3F4F6] text-[#363636] text-[12px] rounded-sm">
-            Active
-          </button>
-          <button className="py-1 px-3 h-8 bg-[#F3F4F6] text-[#363636] text-[12px] rounded-sm">
-            Completed
-          </button>
-        </div>
+  const getFilteredTasks = () => {
+    if (filtered === "active") {
+      return renderInput.filter((item) => !item.isComplete);
+    }
+    if (filtered === "completed") {
+      return renderInput.filter((item) => item.isComplete);
+    }
+    return renderInput;
+  };
 
-        <div className="flex flex-col gap-4">
-          {renderInput.map((item, index) => (
-            <UserInput
-              line={line}
-              setLine={setLine}
-              item={item}
-              text={item.text}
-              key={index}
-              id={index}
-              toggleComplete={toggleComplete}
-              deleteItem={deleteItem}
-            />
-          ))}
-        </div>
-        <TaskInfo renderInput={renderInput} setRenderInput={setRenderInput} />
-      </div>
-    </div>
-  );
-};
+  const filteredTasks = getFilteredTasks();
 
-const TaskInfo = ({ renderInput, setRenderInput }) => {
+  const btnColorChange = (buttonType) => {
+    const base = "py-1 px-3 h-8  text-[12px] rounded-sm";
+    const normal = "bg-[#F3F4F6] text-[#363636]";
+    const active = "bg-[#3c82f6] text-white";
+    return `${base} ${filtered === buttonType ? active : normal}`;
+  };
+
   const itemIsComplete = renderInput.filter((item) => item.isComplete);
 
   const clearCompleted = (isComplete) => {
@@ -116,29 +75,89 @@ const TaskInfo = ({ renderInput, setRenderInput }) => {
   };
 
   return (
+    <div className="min-h-screen w-full flex justify-center">
+      <div>
+        <div className="mt-20  py-6 px-4 w-94.25 min-h-72.5 flex flex-col items-center shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+          <Header add={add} getInput={getInput} input={input} />
+          <CategoryBtn
+            setFiltered={setFiltered}
+            btnColorChange={btnColorChange}
+          />
+          <RenderTasks
+            deleteItem={deleteItem}
+            filteredTasks={filteredTasks}
+            toggleComplete={toggleComplete}
+          />
+          <TaskInfo
+            clearCompleted={clearCompleted}
+            itemIsComplete={itemIsComplete}
+            renderInput={renderInput}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+const Header = ({ input, getInput, add }) => {
+  return (
     <div>
-      {renderInput.length === 0 ? (
-        <div className="text-[#6B7280]  text-[14px] mt-3">
-          No Tasks yet. Add one above!
-        </div>
-      ) : (
-        <div
-          className={`flex w-86.25 h-9.25 pt-4 pb-1 justify-between border-t border-[#E4E4E7] mt-5`}
+      <h1 className="text-[20px] font-semibold">To-Do list</h1>
+      <div className="flex gap-1.5 mb-5 mt-5">
+        <input
+          placeholder="Add a new task..."
+          value={input}
+          onChange={getInput}
+          className="w-70 h-10 py-2 px-4 rounded-sm border border-[#E4E4E7]"
+        />
+        <button
+          onClick={add}
+          className="w-14.75 h-10 text-[13.3px] py-2 px-4 text-white bg-[#3C82F6] rounded-sm tracking-wider shadow-2xs cursor-pointer"
         >
-          <p className="text-[14px] text-[#6B7280]">
-            {itemIsComplete.length} of {renderInput.length} is complete.
-          </p>
-          <button
-            onClick={() => clearCompleted(true)}
-            className="text-[14px] text-[#EF4444] cursor-pointer"
-          >
-            Clear Completed
-          </button>
-        </div>
-      )}
-      <p className="text-[12px] mt-10 text-[#6B7280]">
-        Powered by <span className="text-[#3B73ED]"> Pinecone academy</span>
-      </p>
+          Add
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const CategoryBtn = ({ setFiltered, btnColorChange }) => {
+  return (
+    <div className="flex w-86.25 gap-1.5 mb-5">
+      <button
+        onClick={() => setFiltered("all")}
+        className={btnColorChange("all")}
+      >
+        All
+      </button>
+      <button
+        onClick={() => setFiltered("active")}
+        className={btnColorChange("active")}
+      >
+        Active
+      </button>
+      <button
+        onClick={() => setFiltered("completed")}
+        className={btnColorChange("completed")}
+      >
+        Completed
+      </button>
+    </div>
+  );
+};
+
+const RenderTasks = ({ filteredTasks, toggleComplete, deleteItem }) => {
+  return (
+    <div className="flex flex-col gap-4">
+      {filteredTasks.map((item) => (
+        <UserInput
+          item={item}
+          text={item.text}
+          key={item.id}
+          id={item.id}
+          toggleComplete={toggleComplete}
+          deleteItem={deleteItem}
+        />
+      ))}
     </div>
   );
 };
@@ -169,6 +188,35 @@ const UserInput = ({ toggleComplete, id, item, text, deleteItem }) => {
       ) : (
         ""
       )}
+    </div>
+  );
+};
+
+const TaskInfo = ({ itemIsComplete, clearCompleted, renderInput }) => {
+  return (
+    <div>
+      {renderInput.length === 0 ? (
+        <div className="text-[#6B7280]  text-[14px] mt-3">
+          No Tasks yet. Add one above!
+        </div>
+      ) : (
+        <div
+          className={`flex w-86.25 h-9.25 pt-4 pb-1 justify-between border-t border-[#E4E4E7] mt-5`}
+        >
+          <p className="text-[14px] text-[#6B7280]">
+            {itemIsComplete.length} of {renderInput.length} is complete.
+          </p>
+          <button
+            onClick={() => clearCompleted(true)}
+            className="text-[14px] text-[#EF4444] cursor-pointer"
+          >
+            Clear Completed
+          </button>
+        </div>
+      )}
+      <p className="text-[12px] mt-10 text-[#6B7280]">
+        Powered by <span className="text-[#3B73ED]"> Pinecone academy</span>
+      </p>
     </div>
   );
 };
