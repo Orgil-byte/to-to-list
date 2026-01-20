13fe865640b3d44ff7d6b97907a07967e8e7970a
U.Orgil <ulziitogtohorgil1908@gmail.com>
2026-01-20 09:22:24 +0800

    add component and fix export default page

---
+++ src/app/UI_components/TodoMain.jsx
@@ -0,0 +1,39 @@
+export const TodoMain = () => {
+  const {
+    input,
+    renderInput,
+    filtered,
+    setFiltered,
+    add,
+    getInput,
+    toggleComplete,
+    deleteItem,
+    filteredTasks,
+    btnColorChange,
+    itemIsComplete,
+    clearCompleted,
+  } = useLogicFuncs();
+  return (
+    <div className="min-h-screen w-full flex justify-center">
+      <div>
+        <div className="mt-20  py-6 px-4 w-94.25 min-h-72.5 flex flex-col items-center shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+          <Header add={add} getInput={getInput} input={input} />
+          <CategoryBtn
+            setFiltered={setFiltered}
+            btnColorChange={btnColorChange}
+          />
+          <RenderTasks
+            deleteItem={deleteItem}
+            filteredTasks={filteredTasks}
+            toggleComplete={toggleComplete}
+          />
+          <TaskInfo
+            clearCompleted={clearCompleted}
+            itemIsComplete={itemIsComplete}
+            renderInput={renderInput}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,55 +1,9 @@
-"use client";
-
-import { useLogicFuncs } from "./hooks/Functions";
-import Header from "./UI_components/header/page";
-import CategoryBtn from "./UI_components/categoryBtn/page";
-import RenderTasks from "./UI_components/renderTasks/page";
-import TaskInfo from "./UI_components/taskInfo/page";
-
-export default function Main() {
+const Main = () => {
   return (
     <div>
       <Page />
     </div>
   );
-}
-
-const Page = () => {
-  const {
-    input,
-    renderInput,
-    filtered,
-    setFiltered,
-    add,
-    getInput,
-    toggleComplete,
-    deleteItem,
-    filteredTasks,
-    btnColorChange,
-    itemIsComplete,
-    clearCompleted,
-  } = useLogicFuncs();
-  return (
-    <div className="min-h-screen w-full flex justify-center">
-      <div>
-        <div className="mt-20  py-6 px-4 w-94.25 min-h-72.5 flex flex-col items-center shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
-          <Header add={add} getInput={getInput} input={input} />
-          <CategoryBtn
-            setFiltered={setFiltered}
-            btnColorChange={btnColorChange}
-          />
-          <RenderTasks
-            deleteItem={deleteItem}
-            filteredTasks={filteredTasks}
-            toggleComplete={toggleComplete}
-          />
-          <TaskInfo
-            clearCompleted={clearCompleted}
-            itemIsComplete={itemIsComplete}
-            renderInput={renderInput}
-          />
-        </div>
-      </div>
-    </div>
-  );
 };
+
+export default Main;
